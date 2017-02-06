2a845c704d400e30dc8eb4eac6e23fad70048c99
leesanghyeok <1004xlsrn@gmail.com>
2017-02-06 19:25:59 +0900

    리스트 API 개발 완료

--- Notissu/models.py
+++ Notissu/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
 class Notice(models.Model):
     id = models.AutoField(primary_key=True)
@@ -10,8 +11,9 @@ class Notice(models.Model):
     contents = models.TextField(blank=True, null=True)
     date = models.CharField(max_length=50)
 
+
 class NoticeFiles(models.Model):
     id = models.AutoField(primary_key=True)
-    noticeId = models.ForeignKey('Notice', on_delete=models.CASCADE)
+    notice = models.ForeignKey('Notice', on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     url = models.CharField(max_length=500)
--- Notissu/views.py
+++ Notissu/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+
+from Notissu.models import Notice
+
+RETURN_COUNT = 7
+
+CATEGORY_LIST = {
+    'all': '전체',
+    'student': '학사',
+    'scholarship': '장학',
+    'global': '국제교류',
+    'foreign': '외국인유학생',
+    'recruit': '모집·채용',
+    'inner': '교내행사',
+    'outer': '교외행사',
+    'volunteer': '봉사',
+}
+
+
+def get_list(request, category, page):
+    is_contain = 0
+    for key, value in CATEGORY_LIST.iteritems():
+        if category == key:
+            is_contain = 1
+            continue
+    if is_contain == 0:
+        return wrong_request()
+
+    page = int(page)
+    start_index = (page - 1) * RETURN_COUNT
+    end_index = start_index + RETURN_COUNT
+
+    db_notice_list = Notice.objects.filter(category=CATEGORY_LIST[category]).order_by('-date')[
+                     start_index:end_index].values()
+    if db_notice_list.count() <= 0:
+        return wrong_request()
+
+    return_list = []
+    for dict in db_notice_list:
+        del dict['contents']
+        del dict['category']
+        return_list.append(dict)
+
+    return_list_json = json.dumps(return_list, ensure_ascii=False)
+
+    return HttpResponse(return_list_json, content_type='application/json')
+
+
+def get_view(request, notice_id):
+    return HttpResponse("view " + notice_id, content_type='application/json')
+
+
+def wrong_request():
+    return HttpResponse("{}", content_type='application/json')
